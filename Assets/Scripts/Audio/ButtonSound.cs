80ab513f19e8affce87c105a9d15a008b31c2903
thamenato <thamenato@gmail.com>
2015-07-01 23:58:35 -0300

    Music and refactoring
    Added ambient music to Menu, also click sounds when mouse hover.
    
    Changed some scripts to different folders.

---
+++ Assets/Scripts/Audio/ButtonSound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonSound : MonoBehaviour {
+
+    // sound to be played when mouse hover
+    public AudioClip buttonSound;
+
+    void Start()
+    {
+        if (buttonSound == null)
+            Debug.LogError("ButtonSound at " + this.name + " : no sound defined.");
+    }
+
+    public void playSound()
+    {
+        AudioSource.PlayClipAtPoint(buttonSound, Vector3.one);
+    }
+
+}
