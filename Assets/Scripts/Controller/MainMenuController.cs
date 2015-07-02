402dfcf8aa25fe1900b879326be6b4e8b3075a25
thamenato <thamenato@gmail.com>
2015-07-02 06:22:19 -0300

    Created "About" scroll rect
    Using an image, it is possible to exhibit the "about" on Main Menu

--- Assets/Scripts/Controller/MainMenuController.cs
+++ Assets/Scripts/Controller/MainMenuController.cs
@@ -3,9 +3,22 @@ using System.Collections;
 
 public class MainMenuController : MonoBehaviour {
 
+    public GameObject about;
+
+    void Start()
+    {
+        about.SetActive(false);
+    }
+
     public void startGame()
     {
         Application.LoadLevel("[1st]entrance_corridor");
     }
+
+    public void setAbout(bool c)
+    {
+        about.SetActive(c);
+    }
+
 
 }
